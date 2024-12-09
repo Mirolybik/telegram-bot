42300b5ba8dc2335812cf7f0492cd56df18898c3
Mirolybik <troll7hunter@gmail.com>
2024-12-09 21:01:27 +0300

    123

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
@@ -10,24 +11,26 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+time = time.localtime() 
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     await update.message.reply_html(
         rf"Hi {user.mention_html()}!",
         reply_markup=ForceReply(selective=True),
     )
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Help!")
+#async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#    await update.message.reply_text(update.message.text)
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(update.message.text)
+async def time(update: Update , context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(time)
 
-def main() -> None:
+def main():
     application = Application.builder().token("7871968201:AAGK0JWGwMM7iLvJyNQLILcR5qdR9DhbXQs").build()
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(CommandHandler("time", time))
+#    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
